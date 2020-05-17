9ec19f008e168a89267fbd3d36bc0331e337ebaa
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-05-17 11:31:25 -0300

    test content type

--- tests/users/test_resources_admin.py
+++ tests/users/test_resources_admin.py
@@ -4,9 +4,17 @@ from apps.users.resources_admin import AdminUserPageList
 def test_admin_user_page_list_get_must_be_invocable(client):
     # garante que o a classe tem o metodo get e que ele é invocavel
     atr = getattr(AdminUserPageList, 'get')
+
     assert hasattr(atr, '__call__'), 'o atributo deve ser invocável'
 
 
 def test_admin_user_page_list_get_response_200(client):
     result = client.get('/api/admin/users/page/1')
     assert result.status_code == 200
+
+
+def test_admin_user_page_list_get_response_json_content_type(client):
+    content_type = "application/json"
+    response = client.get('/api/admin/users/page/1')
+
+    assert response.content_type == content_type
